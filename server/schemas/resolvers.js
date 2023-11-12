5febfae02cc323855b5f22b1e0734e57855ad053
Noah Burton <nburton115@gmail.com>
2023-11-11 17:21:59 -0700

    apollo server: server.js. type def /res

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { User, Book} = require('../models');
+
+// Create the functions that fulfill the queries defined in `typeDefs.js`
+
+const resolvers = {
+    Query: {
+        
+    }
+};
+
+// module.exports = moduleHere;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,33 @@
+
+const typeDefs = `
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        bookCount: Int!
+        savedBooks: [Book]
+    }
+
+    type Book {
+       _id: ID!
+       author: String!
+       description: String!
+       bookId: String!
+       image: String
+       link: String
+       title: String!
+    }
+
+    type Query {
+        users: [User]
+        user(_id: ID!): User
+    }
+`;
+//User
+//? update savedBooks
+//* bookCount is a virtual
+
+//todo: update Query
+//todo: add mutations
+module.exports = typeDefs;
