5b3815f6b4036c9b8b3a10213c0385d11a50fd9f
Noah Burton <nburton115@gmail.com>
2023-11-15 14:02:17 -0700

    connection.js mongo private url

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+//! Mongo_private_url might need to be reset
+mongoose.connect(process.env.MONGO_PRIVATE_URL || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
