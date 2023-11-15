371e061e58afefb2a5a36cc7abbb9333dff42c82
Noah Burton <nburton115@gmail.com>
2023-11-15 14:07:35 -0700

    connection js refact

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+// Use the MONGO_PRIVATE_URL environment variable provided by Railway
+const mongoDBUri = process.env.MONGO_PRIVATE_URL || 'mongodb://127.0.0.1:27017/googlebooks';
 
-//! Mongo_private_url might need to be reset
-mongoose.connect(process.env.MONGO_PRIVATE_URL || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose.connection;
